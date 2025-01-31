34fcc277457433fe1e34c169585ef35ba25f628b
Ленка Корнилова <ekornilova@sws.by>
2025-01-31 15:41:41 +0400

    fixed url

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
-import React, { lazy, Suspense, useEffect, useState, useContext, createContext, useReducer } from "react";
-import { createBrowserRouter, RouterProvider, Route, Routes } from "react-router-dom";
+import React, { lazy, Suspense, useEffect, useState} from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Изменили импорт
 import Layout from "./Layout";
 import Movies from "./pages/Movies";
 import { movieTypes, menuItem } from "../constants";
@@ -36,65 +36,52 @@ function App() {
   useEffect(() => {
               localStorage.setItem('userItems', JSON.stringify(userItems));
           }, [userItems]);
-  
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout/>,
-      children: [
-        {
-          index: true,//path: "/",
-          element: <Movies/>
-        },
-        ...movieTypes.map(el =>({
-          path: el.url,
-          element: <Suspense fallback={<LoadingFallback />}><MoviesTopMain/></Suspense>
-        })),
-        ...menuItem.map(el =>({
-          path: el.url,
-          element: <Suspense fallback={<LoadingFallback />}><MoviesTopList/></Suspense>
-        })),
-        {
-          path: "/like",
-          element:  userItems.some((user) => user.session)
-          ?
-           <Suspense fallback={<LoadingFallback />}><LikePage/></Suspense> 
-          :
-          <Suspense fallback={<LoadingFallback />}><Authorization/></Suspense>
-        },
-        {
-          path: "/autorization",
-          element: <Suspense fallback={<LoadingFallback />}><Authorization/></Suspense>
-        },
-        {
-          path: "/registration",
-          element: <Suspense fallback={<LoadingFallback />}><Registration/></Suspense>
-        },
-        {
-          path: "/user",
-          element: userItems.some((user) => user.session)
-          ?  
-          <Suspense fallback={<LoadingFallback />}><UserPage/></Suspense> 
-          : 
-          <Suspense fallback={<LoadingFallback />}><Authorization/></Suspense>
-        },
-        {
-          path: "/movie/:id",
-          element: <Suspense fallback={<LoadingFallback />}><MoviesDetail/></Suspense>
-        },
-        {
-          path: "*",
-          element: <NotFoundPage/>
-        },
-      ]
-    },
-  ]
-);
-  return ( 
-      <AppContext.Provider value={{movieItems, setMovieItems, userItems, setUserItems}} >
-          <RouterProvider router={router}/>
-        </AppContext.Provider>
-  )
+
+  return (
+    <AppContext.Provider value={{ movieItems, setMovieItems, userItems, setUserItems }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Movies />} />
+            {movieTypes.map(el => (
+              <Route key={el.url} path={el.url} element={
+                <Suspense fallback={<LoadingFallback />}>
+                  <MoviesTopMain />
+                </Suspense>
+              } />
+            ))}
+            {menuItem.map(el => (
+              <Route key={el.url} path={el.url} element={
+                <Suspense fallback={<LoadingFallback />}>
+                  <MoviesTopList />
+                </Suspense>
+              } />
+            ))}
+            <Route path="/like" element={
+              userItems.some((user) => user.session)
+                ? <Suspense fallback={<LoadingFallback />}><LikePage /></Suspense>
+                : <Suspense fallback={<LoadingFallback />}><Authorization /></Suspense>
+            } />
+            <Route path="/autorization" element={
+              <Suspense fallback={<LoadingFallback />}><Authorization /></Suspense>
+            } />
+            <Route path="/registration" element={
+              <Suspense fallback={<LoadingFallback />}><Registration /></Suspense>
+            } />
+            <Route path="/user" element={
+              userItems.some((user) => user.session)
+                ? <Suspense fallback={<LoadingFallback />}><UserPage /></Suspense>
+                : <Suspense fallback={<LoadingFallback />}><Authorization /></Suspense>
+            } />
+            <Route path="/movie/:id" element={
+              <Suspense fallback={<LoadingFallback />}><MoviesDetail /></Suspense>
+            } />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AppContext.Provider>
+  );
 }
 
 export default App;
--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-export default function ErrorMessage() {
+export default function NotFoundPage() {
     const { t } = useTranslation();
   
   return (
